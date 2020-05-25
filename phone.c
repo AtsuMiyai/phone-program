8d7106dcacc5530069073f9ae6c38e603d0c8138
chibechan <65175432+chibechan@users.noreply.github.com>
2020-05-25 23:29:31 +0900

    Update phone.c

--- phone.c
+++ phone.c
@@ -1,4 +1,9 @@
-//./phone [ポート番号]  :サーバー側　　./phone [IPアドレス] [ポート番号] :クライアント側
+/*
+音声同時通話プログラム
+
+./i1i2i3_phone [ポート番号]  :サーバー側　　./i1i2i3_phone [IPアドレス] [ポート番号]　:クライアント側
+
+*/
 #include <netinet/in.h>
 #include <netinet/ip.h>
 #include <netinet/tcp.h>
@@ -33,17 +38,15 @@ int main(int argc, char **argv)
         int s = accept(ss, (struct sockaddr *)&client_addr, &len);
         close(ss);
         FILE *rec;
-        //char buf[1024];
-        char *cmd1 = "rec -t raw -b 16 -c 1 -e s -r 44100 -";
-        rec = popen(cmd1, "r"); //クライアントからの接続を受け付けてから録音を始める。
-        //ここまでがサーバーだけの役割
+        char *cmd1 = "rec --buffer 512 -t raw -b 16 -c 1 -e s -r 44100 -";
+        rec = popen(cmd1, "r");
         if (rec == NULL)
         {
             perror("open");
             exit(1);
         }
 
-        char *cmd2 = "play -t raw -b 16 -c 1 -e s -r 44100 - ";
+        char *cmd2 = "play --buffer 512 -t raw -b 16 -c 1 -e s -r 44100 - ";
         FILE *play;
         play = popen(cmd2, "w");
         if (play == NULL)
@@ -51,11 +54,9 @@ int main(int argc, char **argv)
             perror("open");
             exit(1);
         }
-        //char store[1];
-        // char data1[1];
         short store[N];
         short data1[N];
-        while (1) //fpは録音を読んでる。
+        while (1)
         {
 
             int n = fread(store, 2, N, rec); //標準出力を読み取っている。
@@ -65,11 +66,11 @@ int main(int argc, char **argv)
                 perror("send");
                 exit(1);
             }
-            if (n==0){
+            if (n == 0)
+            {
                 break;
             }
-
-            write(s, store, 2 * N);
+            write(s, store, 2 * n);
             int o = read(s, data1, 2 * N); //クライアント側からの書き込みを標準入力に書き出す。
             if (o == -1)
             {
@@ -77,7 +78,11 @@ int main(int argc, char **argv)
                 perror("recv");
                 exit(1);
             }
-            fwrite(data1, 2, N, play);
+            if (o == 0)
+            {
+                break;
+            }
+            fwrite(data1, 2, o / 2, play);
         }
 
         pclose(rec);
@@ -88,8 +93,6 @@ int main(int argc, char **argv)
     {
         /*ここからはクライアントの役割*/
         int s = socket(PF_INET, SOCK_STREAM, 0);
-        //FILE *fp;
-        //gets()とか？何かしらの形で読み取っては、渡すようにすれば良い、これは簡単にできるはず。
         if (s == -1)
         {
             perror("open");
@@ -105,11 +108,7 @@ int main(int argc, char **argv)
             perror("connect");
             exit(1);
         }
-        if (ret == 0)
-        {
-            perror("coct");
-            //exit(1);
-        }
+
         char *cmdline_r = "rec -t raw -b 16 -c 1 -e s -r 44100 - ";
         FILE *rec;
         rec = popen(cmdline_r, "r");
@@ -128,34 +127,35 @@ int main(int argc, char **argv)
         }
         int n;
         int i = 0;
-
         short data1[N];
         short store1[N];
         while (1)
         {
 
-           
             int n = read(s, data1, 2 * N);
             if (n == -1)
             {
                 perror("read");
                 exit(1);
             }
-            fwrite(data1, 2, N, play);
-            //freadから読み取るべき
+            if (n == 0)
+            {
+                break;
+            }
+            fwrite(data1, 2, n / 2, play);
             int n_ = fread(store1, 2, N, rec);
             if (n_ == -1)
             {
                 perror("fread");
                 exit(1);
             }
-            if (n==0){
+            if (n_ == 0)
+            {
                 break;
             }
-
-            write(s, store1, 2 * N);
+            write(s, store1, 2 * n);
             memset(data1, 0, sizeof(data1));
-            memset(store1, 0, sizeof(store1)); 
+            memset(store1, 0, sizeof(store1));
         }
         pclose(rec);
         pclose(play);
